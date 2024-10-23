d13ecef01ec94bcb7a813ab2e498585f98a31c98
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-23 08:16:55 +0200

    Started the class

---
+++ Labb2_Animal/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Animal
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int NumberOfLegs { get; set; }
+        public bool Meateater { get; set; }
+        public string Habitat { get; set; }
+    }
+
+    public Animal Eat()
+    {
+        Console.WriteLine($"{Name} äter sin mat.");
+    }
+    public Animal Sleep()
+    {
+        Console.WriteLine();
+    }
+    public Animal Hunt()
+    {
+
+    }
+}
