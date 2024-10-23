f69e2cfd0e6e084f063dab9bd618e45977b19e8d
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-23 08:09:54 +0200

    Add project files.

---
+++ Labb2_Animal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb2_Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
