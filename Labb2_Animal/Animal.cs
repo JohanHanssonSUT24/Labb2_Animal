d3eb2a1a82ba8ae83a9944f6097e845b28937e48
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-10-29 19:56:05 +0100

    Added default-values

--- Labb2_Animal/Animal.cs
+++ Labb2_Animal/Animal.cs
@@ -8,23 +8,59 @@ namespace Labb2_Animal
 {
     public class Animal
     {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public int NumberOfLegs { get; set; }
-        public bool Meateater { get; set; }
-        public string Habitat { get; set; }
-    }
+        public string Name { get; set; } = "Ej angivet";
+        public int Age { get; set; } = 1;
+        public int NumberOfLegs { get; set; } = 2;
+        public bool Meateater { get; set; } = true;
+        public string Habitat { get; set; } = "djungel";
+        public string Sound { get; set; } = "Roooaaaarr";
 
-    public Animal Eat()
-    {
-        Console.WriteLine($"{Name} äter sin mat.");
-    }
-    public Animal Sleep()
-    {
-        Console.WriteLine();
-    }
-    public Animal Hunt()
-    {
 
+        public Animal(string name, int age, int numberOfLegs, bool meateater, string habitat, string sound)
+        {
+            Name = name;
+            Age = age;
+            NumberOfLegs = numberOfLegs;
+            Meateater = meateater;
+            Habitat = habitat;
+            Sound = sound;
+        }
+        public Animal()
+        {
+
+        }
+        public void Eat()
+        {
+            if (Meateater == true)
+            {
+                Console.WriteLine($"{Name} äter kött!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} äter inte kött.");
+            }
+        }
+        public void Sleep()
+        {
+            Console.WriteLine($"Pga sin ålder om {Age} år så måste hen vila. ");
+        }
+        public void Hunt()
+        {
+            Console.WriteLine($"Med sina {NumberOfLegs} ben jagar den sitt byte i sin naturliga miljö, {Habitat}.");
+        }
+        public void Sounds()
+        {
+            Console.WriteLine($"{Name} har ett läte som låter såhär {Sound}");
+        }
+
+        public void PrintAnimal()
+        {
+            Console.WriteLine($"Namn: {Name}");
+            Console.WriteLine($"Ålder: {Age}");
+            Console.WriteLine($"Antal ben: {NumberOfLegs}");
+            Console.WriteLine($"Äter kött: {Meateater}");
+            Console.WriteLine($"Hemvist: {Habitat}");
+            Console.WriteLine($"Läte: {Sound}");
+        }
     }
 }
---
+++ Labb2_Animal/Horse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_Animal
+{
+    public class Horse : Animal
+    {
+        public int Rodeo { get; set; } = 8;
+        public Horse()
+        {
+
+        }
+        public Horse(int rodeo, string name, int age, int numberOfLegs, bool meateater, string habitat, string sound) : base(name, age, numberOfLegs, meateater, habitat, sound)
+        {
+            Rodeo = rodeo;
+        }
+        
+
+        public void HowLong()
+        {
+            Console.WriteLine($"Hur länge skulle du klara av att sitta kvar på hästen under en rodeo? {Rodeo} sekunder.");
+        }
+    }
+}
