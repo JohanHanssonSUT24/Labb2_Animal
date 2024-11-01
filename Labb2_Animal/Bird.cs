6d9f1ab07ca7139143de2c05d32b15df3da7a5af
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-01 15:42:17 +0100

    Comments almost done

--- Labb2_Animal/Bird.cs
+++ Labb2_Animal/Bird.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace Labb2_Animal
 {
+    //Subclass to Animal, "Bird"
     public class Bird : Animal
     {
         public bool CanFly { get; set; } = true;
         public Bird(bool canfly, string name, int age, int numberOfLegs, bool meateater, string habitat, string sound) : base(name, age, numberOfLegs, meateater, habitat, sound)
         {
-            CanFly = canfly;
-            
+            CanFly = canfly;           
         }
         public Bird()
         {
 
         }
-
+        //Unique method for the animal
         public void WillItFly()
         {
             Console.WriteLine($"It is {CanFly} that this bird will fly if you throw it of a cliff...");
         }
+        //Override to print unique sound
         public override void MakeSound()
         {
             Console.WriteLine($"You can hear the {SoundsLike} in the distance!");
         }
-
         public override void Hunt()
         {
             Console.WriteLine($"{Name} swoops in on the attack.");
